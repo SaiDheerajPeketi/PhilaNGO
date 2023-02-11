d6c7446d6943a7bc685bd9f9f6ad84fc5402e79b
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-02-11 07:31:18 +0530

    All Login Pages Added

---
+++ app/src/main/java/com/example/philango/ContributorSignUp.java
@@ -0,0 +1,14 @@
+package com.example.philango;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContributorSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contributor_sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/philango/NgoSignUp.java
@@ -0,0 +1,14 @@
+package com.example.philango;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NgoSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngo_sign_up);
+    }
+}
