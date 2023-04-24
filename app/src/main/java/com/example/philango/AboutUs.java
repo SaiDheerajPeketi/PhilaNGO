71a7619a5e25c4f52912efd213a631557ee4e27c
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-04-24 12:17:53 +0530

    Modify About Us

--- app/src/main/java/com/example/philango/AboutUs.java
+++ app/src/main/java/com/example/philango/AboutUs.java
@@ -1,18 +1,28 @@
 package com.example.philango;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class AboutUs extends AppCompatActivity {
-    private RecyclerView aboutUsRecyclerView;
+    private ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
-        aboutUsRecyclerView = findViewById(R.id.aboutUsRecyclerView);
-        aboutUsRecyclerView.setHasFixedSize(true);
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLoginRegister = new Intent(AboutUs.this,LoginRegisterActivity.class);
+                startActivity(toLoginRegister);
+                finish();
+            }
+        });
     }
 }
